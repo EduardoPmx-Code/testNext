13ee446a0c338887eef0c47ce8359ffc73b33979
Eduardo Pimentel <eduardo.Pimentel@totalabogados.cl>
2022-08-16 18:27:55 -0400

    first commit

--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
-
+const withPreact = require('next-plugin-preact')
+module.exports = withPreact({
+ // all your other Next.js options
+})
+module.exports =  withPreact
 module.exports = nextConfig
